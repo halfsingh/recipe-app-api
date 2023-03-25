86d1f7490e02e484e8ffab3f3a776525fb8ef6a6
halfsingh <vijayksingh.1998@gmail.com>
2023-03-25 18:49:12 +0530

    added tests for token

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -2,8 +2,12 @@
 Serializers for the user api view
 """
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate
+)
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
